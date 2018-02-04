a419f44097ea01a3cfde36845c33b055b3171f87
Albilaga L. Pradana <albilaga.lp@gmail.com>
2018-02-04 23:02:53 +0700

    add example

---
+++ Example/ViewPageriOSSample/ViewPageriOSSample/ImagesDataSource.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UIKit;
+using ViewPager.iOS;
+
+namespace ViewPageriOSSample
+{
+	public class ImagesDataSource : ViewPagerDataSource
+	{
+		IReadOnlyList<string> _Datas;
+		public ImagesDataSource(IReadOnlyList<string> datas)
+		{
+			this._Datas = datas;
+		}
+
+		public override void ItemSelected(int index)
+		{
+			//throw new NotImplementedException();
+		}
+
+		public override int NumberOfItems(ViewPager.iOS.ViewPager viewPager)
+		{
+			return _Datas.Count;
+		}
+
+		public override UIView ViewAtIndex(ViewPager.iOS.ViewPager viewPager, int index, UIView view = null)
+		{
+			return new UILabel
+			{
+				Text = _Datas[index],
+				ContentMode=UIViewContentMode.Center,
+				TextAlignment=UITextAlignment.Center
+			};
+		}
+	}
+}
---
+++ Example/ViewPageriOSSample/ViewPageriOSSample/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using UIKit;
+
+namespace ViewPageriOSSample
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			var viewPager = new ViewPager.iOS.ViewPager(this.View.Frame);
+			this.View.Add(viewPager);
+			var dataSource = new ImagesDataSource(new List<string>
+			{
+				"View Pager 1","View Pager 2","View Pager 3"
+			});
+			viewPager.DataSource = dataSource;
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
